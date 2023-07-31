9d3e3fe9d0c52eb04de97b2f1f9d2bca0985d772
feliperod0519 <feliperod0519@hotmail.com>
2023-07-31 15:48:03 -0400

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  
+  title = 'Felipe-Rod';
+  users: any;
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7246/api/users").subscribe(d=>{
+      this.users=d;
+      console.log(this.users);
+    });
+  }
 }
